b78a0ae19c4c99778517d9ebe602c238cbf5e214
BaherMh <105556066+BaherMh@users.noreply.github.com>
2022-11-05 22:46:34 +0200

    UOW

--- AppDbContext/UOW/IUnitOfWork.cs
+++ AppDbContext/UOW/IUnitOfWork.cs
@@ -7,8 +7,18 @@ namespace AppDbContext.UOW
 {
     public interface IUnitOfWork
     {
-        public IStudentRepo StudentRepo { get; set; }
+        public IAttributeRepo AttributeRepo { get; set; }
+        public ICategoryRepo CategoryRepo { get; set; }
+        public IProductRepo ProductRepo { get; set; }
+        public ICustomerRepo CustomerRepo { get; set; }
+        public IOrderRepo OrderRepo { get; set; }
+        public IProductOrderRepo ProductOrderRepo { get; set; }
+        public IShippingRepo ShippingRepo { get; set; }
+        public IValueDateRepo ValueDateRepo { get; set; }
+        public IValueIntRepo ValueIntRepo { get; set; }
+        public IValueVarcharRepo ValueVarcharRepo { get; set; }
 
+        public IStudentRepo StudentRepo { get; set; }
         public void SaveChanges ();
 
         public void RollBack();
--- AppDbContext/UOW/UnitOfWork.cs
+++ AppDbContext/UOW/UnitOfWork.cs
@@ -9,8 +9,17 @@ namespace AppDbContext.UOW
 {
     public class UnitOfWork : IUnitOfWork
     {
+        public IAttributeRepo AttributeRepo { get; set; }
+        public ICategoryRepo CategoryRepo { get; set; }
+        public IProductRepo ProductRepo { get; set; }
+        public ICustomerRepo CustomerRepo { get; set; }
+        public IOrderRepo OrderRepo { get; set; }
+        public IProductOrderRepo ProductOrderRepo { get; set; }
+        public IShippingRepo ShippingRepo { get; set; }
+        public IValueDateRepo ValueDateRepo { get; set; }
+        public IValueIntRepo ValueIntRepo { get; set; }
+        public IValueVarcharRepo ValueVarcharRepo { get; set; }
         public IStudentRepo StudentRepo { get; set; }
-
         protected readonly TestDbContext _db;
 
         public UnitOfWork(TestDbContext db)
